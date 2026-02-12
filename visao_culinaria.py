73e4ab211b73853af97b81be4c7014a98d0237de
Álvaro Morais <alvaromorais.arq@gmail.com>
2026-02-11 23:19:37 -0300

    Creating a Streamlit page for the app visao_culinaria.py

--- visao_culinaria.py
+++ visao_culinaria.py
@@ -4,6 +4,8 @@ import folium
 from folium.plugins import MarkerCluster
 import plotly.graph_objects as go
 import plotly.express as px
+import streamlit as st
+from streamlit_folium import st_folium
 
 #carregando os dados
 df = pd.read_csv('/home/alvaro/Documentos/alvaro/comunidadeds/projetos/projeto_zomato/dataset/zomato.csv')
@@ -64,27 +66,78 @@ df1 = df1.drop(index=5566)
 # Reindexar o DataFrame após a remoção, para manter os índices sequenciais se necessário.
 df1 = df1.reset_index(drop=True)
 
-#===========Inicio do projeto=================
+#===========================================
+#BARRA LATERAL NO STREAMLIT
+#===========================================
+#configurando a página
+st.set_page_config(page_title='Visão Culinária', layout='wide')
+
+#criando a barra lateral
+st.sidebar.markdown("# Filtros")
+st.sidebar.markdown("### Selecione os países para visualizar o dados:")
+st.sidebar.markdown("""---""")
+
+country_options = st.sidebar.multiselect("Escolha os países:",
+                      options=df1['Country name'].unique().tolist(),
+                      default=df1['Country name'].unique().tolist(),
+                      key='paises')
+#filtrando os dados com base na seleção do usuário
+linhas_selecionadas = df1['Country name'].isin(country_options)
+
+df1 = df1.loc[linhas_selecionadas, :]
+
+#===========================================
+#LAYOUT NO STREAMLIT
+#===========================================
+with st.container():
+    st.markdown("# VISÃO CULINÁRIA")
+    st.markdown("## Top restaurantes")
+    st.markdown("##### Os restaurantes com mais de 1500 votos e melhores avaliações")
 #Top restaurants
-top_rest = df1.loc[:,['Restaurant Name', 'Country name', 'City','Cuisines', 'Currency','Average Cost for two' ,'Aggregate rating', 'Votes']].sort_values('Aggregate rating', ascending = False)
-top_rest
+    votos_rest = df1.loc[df1['Votes']>1500,['Restaurant Name', 'Country name', 'City','Cuisines', 'Currency','Average Cost for two' ,'Aggregate rating', 'Votes']]
+    top_rest = votos_rest.loc[:,['Restaurant Name', 'Country name', 'City','Cuisines', 'Currency','Average Cost for two' ,'Aggregate rating', 'Votes']].sort_values(['Aggregate rating', 'Votes'], ascending = False)
+    st.dataframe(top_rest)
 
 #------------------------------------------------
+with st.container():
+    st.markdown("""---""")
+    st.markdown("## Top 10 culinárias com as melhores avaliações médias")
+    st.markdown("##### Melhores avaliações médias e maiores quantidade de votos  ")
 # Analisando as 10 culinárias com as melhores avaliações médias
-top10_cuisines = df1.loc[:,['Cuisines','Aggregate rating']].groupby('Cuisines').mean('Aggregate rating').sort_values('Aggregate rating', ascending=False).reset_index().head(10)
+    top10_cuisines = df1.loc[:,['Cuisines','Country name','Votes', 'Aggregate rating']].groupby('Cuisines').mean('Aggregate rating').sort_values(['Aggregate rating', 'Votes'], ascending=False).reset_index().head(10)
 
-fig = px.bar(top10_cuisines, x= 'Cuisines', y='Aggregate rating', labels={'Cuisines':'Culinária', 'Aggregate rating':'Ranking'})
-fig
+    fig = px.bar(top10_cuisines, x= 'Cuisines', y='Aggregate rating', labels={'Cuisines':'Culinária', 'Aggregate rating':'Ranking'}, color='Votes')
+    st.plotly_chart(fig, use_container_width=True)
+    st.dataframe(top10_cuisines)
 
 #------------------------------------------------
+with st.container():
+    st.markdown("""---""")
+    st.markdown("## Top 10 culinárias com as piores avaliações médias")
+    st.markdown("##### Piores avaliações médias e maiores quantidade de votos  ")
 # Analisando as 10 culinárias com as piores avaliações médias
 
 # Filter out rows where 'Aggregate rating' is 0
-filtered_df1 = df1.loc[df1['Aggregate rating'] != 0]
+    filtered_df1 = df1.loc[(df1['Aggregate rating'] != 0) & (df1['Votes'] > 100)]
 
 # Group by 'Cuisines' and calculate the mean of 'Aggregate rating'
-piores_cuisines = filtered_df1.groupby('Cuisines')['Aggregate rating'].mean().sort_values(ascending=False).reset_index().tail(10)
+    #piores_cuisines = filtered_df1.groupby('Cuisines')['Aggregate rating'].mean().sort_values(['Aggregate rating','Votes'], ascending=False).reset_index().tail(10)
+    piores_cuisines = (
+    filtered_df1
+    .groupby('Cuisines')
+    .agg({
+        'Aggregate rating': 'mean',
+        'Votes': 'sum'   # ou sum, se fizer mais sentido
+    })
+    .sort_values(
+        by=['Aggregate rating', 'Votes'],
+        ascending=[False, False]
+    )
+    .reset_index()
+    .tail(10)
+)
 
-fig = px.bar(piores_cuisines, x= 'Cuisines', y='Aggregate rating', labels={'Cuisines':'Culinária', 'Aggregate rating':'Ranking'})
-fig
+    fig = px.bar(piores_cuisines, x= 'Cuisines', y='Aggregate rating', labels={'Cuisines':'Culinária', 'Aggregate rating':'Ranking', 'Votes':'Quantidade de votos'}, color='Aggregate rating', color_continuous_scale=px.colors.sequential.Reds[::-1])
+    st.plotly_chart(fig, use_container_width=True)
+    st.dataframe(piores_cuisines)
 
